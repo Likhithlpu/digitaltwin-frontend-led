87c03ca59c1caca47036a244ea5ceef203dfb3d3
Likhith Kanigolla <likhithkanigolla@gmail.com>
2023-11-22 11:03:35 +0530

    updted url for node 2

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ function fetchLedColor() {
   
   async function fetchData() {
     try {
-      const response = await fetch('http://localhost:3486/latest-data');
+      const response = await fetch('http://192.168.0.109:3486/latest-data-node2');
       const data = await response.json();
   
       // Extract RGB values
